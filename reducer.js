3afc05aaaec9e1267507186e9b37dc75222722ec
Anthony <qdouble@gmail.com>
2016-09-02 14:18:51 -0700

    init

---
+++ reducer.js
@@ -0,0 +1,25 @@
+"use strict";
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+var BehaviorSubject_1 = require('rxjs/BehaviorSubject');
+var Reducer = (function (_super) {
+    __extends(Reducer, _super);
+    function Reducer(_dispatcher, initialReducer) {
+        _super.call(this, initialReducer);
+        this._dispatcher = _dispatcher;
+    }
+    Reducer.prototype.replaceReducer = function (reducer) {
+        this.next(reducer);
+    };
+    Reducer.prototype.next = function (reducer) {
+        _super.prototype.next.call(this, reducer);
+        this._dispatcher.dispatch({ type: Reducer.REPLACE });
+    };
+    Reducer.REPLACE = '@ngrx/store/replace-reducer';
+    return Reducer;
+}(BehaviorSubject_1.BehaviorSubject));
+exports.Reducer = Reducer;
+//# sourceMappingURL=reducer.js.map
